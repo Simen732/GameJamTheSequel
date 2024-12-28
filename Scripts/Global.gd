f9cc7ce928bac4b5efd3adcd0545ef9d5f7494d4
Blank <140179536+BlankTuber@users.noreply.github.com>
2024-12-28 18:29:54 +0100

    Added music and updated global var

--- Scripts/Global.gd
+++ Scripts/Global.gd
@@ -7,5 +7,14 @@ var unlocks := {
 	"grapplingHook": false,
 	"teleport": false,
 	"light": false,
-	
+	"pickaxe": false,
+	"brush": false
+}
+
+var stages := {
+	"pyramid": false,
+	"room1": false,
+	"room2": false,
+	"room3": false,
+	"boss": false
 }
