937dd494ff5135a59660ed994ab60bd6fcda0988
Blank <140179536+BlankTuber@users.noreply.github.com>
2024-12-29 22:23:53 +0100

    Fixed wallrun emit cuz it don exist

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -122,7 +122,6 @@ func _on_area_3d_area_entered(area: Area3D) -> void:
 		
 	if area.get_parent().name == "WallRunningUnlock":
 		print("Du har nå WallRunningUnlock")
-		Global.emit_signal("WallrunUnlocked")
 		Global.unlocks.wallrun = true
 		area.get_parent().queue_free()
 		
