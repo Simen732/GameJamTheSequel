516b290c9b1779b4ef6eec3cbb41e420c6f58e43
Blank <140179536+BlankTuber@users.noreply.github.com>
2024-12-30 12:22:00 +0100

    Updated a bit

--- Scripts/Extra/touchable.gd
+++ Scripts/Extra/touchable.gd
@@ -1,8 +1,8 @@
 extends Area3D
-@onready var timer: Timer = $Timer
-@onready var character_body_3d: CharacterBody3D = $"../CharacterBody3D"
-@onready var timer_2: Timer = $Timer2
 
+@onready var cool_view: Control = $CoolView
+@onready var timer: Timer = $CoolView/Timer
+@onready var animation_player: AnimationPlayer = $CoolView/AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -21,16 +21,4 @@ func _on_area_entered(area: Area3D) -> void:
 
 
 func _on_timer_timeout() -> void:
-	$"../CoolIntermissionCamera/Camera3D/CoolThingy/AnimationPlayer".play("Fade")
-	$"../CoolIntermissionCamera/Camera3D".current = true
-	character_body_3d.get_child(1).current = false
-	$"../CoolIntermissionCamera/Camera3D/CoolThingy".visible = true
-	timer_2.wait_time = 1.85
-	timer_2.one_shot = true
-	timer_2.start()
-
-
-func _on_timer_2_timeout() -> void:
-	$"../CoolIntermissionCamera/Camera3D".current = false
-	character_body_3d.get_child(1).current = true
-	$"../CoolIntermissionCamera/Camera3D/CoolThingy".visible = false
+	animation_player.play("fade")
